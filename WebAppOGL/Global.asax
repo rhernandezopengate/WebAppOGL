bc8db9de4092b9cf61fea4d129e24046f7a7c85e
rhernandezopengate <43422446+rhernandezopengate@users.noreply.github.com>
2021-12-19 13:24:27 -0600

    Agregar archivos de proyecto.

---
+++ WebAppOGL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppOGL.MvcApplication" Language="C#" %>
---
+++ WebAppOGL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppOGL.Startup))]
+namespace WebAppOGL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
