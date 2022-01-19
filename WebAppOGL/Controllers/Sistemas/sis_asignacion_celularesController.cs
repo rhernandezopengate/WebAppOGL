3fb21feec05099fe0b570a5d58e36f7af07f4ec9
rhernandezopengate <43422446+rhernandezopengate@users.noreply.github.com>
2022-01-19 17:03:00 -0600

    19/01/2021

--- WebAppOGL/Controllers/Sistemas/sis_asignacion_celularesController.cs
+++ WebAppOGL/Controllers/Sistemas/sis_asignacion_celularesController.cs
@@ -8,24 +8,109 @@ using System.Web;
 using System.Web.Mvc;
 using WebAppOGL.Entities.Administracion;
 using WebAppOGL.Entities.Sistemas;
+using System.Linq.Dynamic;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WebAppOGL.Controllers.Sistemas
 {
     public class sis_asignacion_celularesController : Controller
     {
         private db_a3f19c_administracionEntities db = new db_a3f19c_administracionEntities();
+        private db_a3f19c_administracionEntities1 dbAdmin = new db_a3f19c_administracionEntities1();
 
         public ActionResult AsignacionCelularesParcial() 
         {
             return View();
         }
 
-
         // GET: sis_asignacion_celulares
         public ActionResult Index()
         {
             return View(db.sis_asignacion_celulares.ToList());
         }
+
+
+        [HttpPost]
+        public ActionResult ObtenerAsignacionCelulares()
+        {
+            try
+            {
+                var Draw = Request.Form.GetValues("draw").FirstOrDefault();
+                var Start = Request.Form.GetValues("start").FirstOrDefault();
+                var Length = Request.Form.GetValues("length").FirstOrDefault();
+                var SortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][data]").FirstOrDefault();
+                var SortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+
+                var nombreempleado = Request.Form.GetValues("columns[0][search][value]").FirstOrDefault();
+
+                int PageSize = Length != null ? Convert.ToInt32(Length) : 0;
+                int Skip = Start != null ? Convert.ToInt32(Start) : 0;
+                int TotalRecords = 0;
+
+                List<AsignacionCelularesViewModel> lista = new List<AsignacionCelularesViewModel>();
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString()))
+                {
+                    con.Open();
+
+                    string sql = "exec SP_AsignacionCelulares_Index @nombreempleado";
+                    var query = new SqlCommand(sql, con);
+
+                    if (nombreempleado != "")
+                    {
+                        query.Parameters.AddWithValue("@nombreempleado", nombreempleado);
+                    }
+                    else
+                    {
+                        query.Parameters.AddWithValue("@nombreempleado", DBNull.Value);
+                    }
+
+                    using (var dr = query.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            // facturas
+                            var asignacion = new AsignacionCelularesViewModel();
+
+                            asignacion.Id = Convert.ToInt32(dr["Id"]);
+                            asignacion.Area = dr["Area"].ToString();
+                            asignacion.Empleado = dr["Empleado"].ToString();
+                            asignacion.Sucursal = dr["Sucursal"].ToString();
+                            asignacion.Cuenta = dr["Cuenta"].ToString();
+                            asignacion.ModeloCelular = dr["Celular"].ToString();
+
+                            if (dr["FechaAlta"].ToString() != "")
+                            {
+                                asignacion.FechaAlta = Convert.ToDateTime(dr["FechaAlta"].ToString()).Date.ToShortDateString();
+                            }
+                            else
+                            {
+                                asignacion.FechaAlta = "";
+                            }
+
+                            lista.Add(asignacion);
+                        }
+                    }
+                }
+
+                if (!(string.IsNullOrEmpty(SortColumn) && string.IsNullOrEmpty(SortColumnDir)))
+                {
+                    lista = lista.OrderBy(SortColumn + " " + SortColumnDir).ToList();
+                }
+
+                TotalRecords = lista.ToList().Count();
+                var NewItems = lista.Skip(Skip).Take(PageSize == -1 ? TotalRecords : PageSize).ToList();
+
+                return Json(new { draw = Draw, recordsFiltered = TotalRecords, recordsTotal = TotalRecords, data = NewItems }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception _ex)
+            {
+                Console.WriteLine(_ex.Message.ToString());
+                return null;
+            }
+        }
+
 
         // GET: sis_asignacion_celulares/Details/5
         public ActionResult Details(int? id)
@@ -45,6 +130,12 @@ namespace WebAppOGL.Controllers.Sistemas
         // GET: sis_asignacion_celulares/Create
         public ActionResult Create()
         {
+            ViewBag.sis_celulares_Id = new SelectList(db.sis_celulares, "Id", "IMEI");
+            ViewBag.adm_empleados_Id = new SelectList(dbAdmin.adm_empleados, "Id", "Nombres");
+            ViewBag.adm_area_Id = new SelectList(dbAdmin.adm_area, "Id", "Descripcion");
+            ViewBag.adm_sucursales_Id = new SelectList(dbAdmin.adm_sucursales, "Id", "Descripcion");
+            ViewBag.adm_cuentas_Id = new SelectList(dbAdmin.adm_cuentas, "Id", "Descripcion");
+
             return View();
         }
 
@@ -59,10 +150,10 @@ namespace WebAppOGL.Controllers.Sistemas
             {
                 db.sis_asignacion_celulares.Add(sis_asignacion_celulares);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Json("Correcto", JsonRequestBehavior.AllowGet);
             }
 
-            return View(sis_asignacion_celulares);
+            return Json("Error", JsonRequestBehavior.AllowGet);
         }
 
         // GET: sis_asignacion_celulares/Edit/5
@@ -77,6 +168,13 @@ namespace WebAppOGL.Controllers.Sistemas
             {
                 return HttpNotFound();
             }
+
+            ViewBag.sis_celulares_Id = new SelectList(db.sis_celulares, "Id", "IMEI", sis_asignacion_celulares.sis_celulares_Id);
+            ViewBag.adm_empleados_Id = new SelectList(dbAdmin.adm_empleados, "Id", "Nombres", sis_asignacion_celulares.adm_empleados_Id);
+            ViewBag.adm_area_Id = new SelectList(dbAdmin.adm_area, "Id", "Descripcion", sis_asignacion_celulares.adm_area_Id);
+            ViewBag.adm_sucursales_Id = new SelectList(dbAdmin.adm_sucursales, "Id", "Descripcion", sis_asignacion_celulares.adm_sucursales_Id);
+            ViewBag.adm_cuentas_Id = new SelectList(dbAdmin.adm_cuentas, "Id", "Descripcion", sis_asignacion_celulares.adm_cuentas_Id);
+
             return View(sis_asignacion_celulares);
         }
 
@@ -91,9 +189,9 @@ namespace WebAppOGL.Controllers.Sistemas
             {
                 db.Entry(sis_asignacion_celulares).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return Json("Correcto", JsonRequestBehavior.AllowGet);
             }
-            return View(sis_asignacion_celulares);
+            return Json("Error", JsonRequestBehavior.AllowGet);
         }
 
         // GET: sis_asignacion_celulares/Delete/5
---
+++ WebAppOGL/Entities/Sistemas/AsignacionCelularesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppOGL.Entities.Sistemas
+{
+    public class AsignacionCelularesViewModel
+    {
+        public int Id { get; set; }
+
+        public string FechaAlta { get; set; }
+
+        public string Area { get; set; }
+
+        public string Empleado { get; set; }
+
+        public string Sucursal { get; set; }
+
+        public string Cuenta { get; set; }
+
+        public string ModeloCelular { get; set; }
+    }
+
+    public partial class sis_asignacion_celulares 
+    {
+        
+    }
+}
